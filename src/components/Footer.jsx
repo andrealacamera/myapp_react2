3eddd3b730da74e42c41ae73a2b31e074850ace2
andrealacamera <andrea.lacamera0@gmail.com>
2022-07-29 19:29:13 +0200

    removed info from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,12 @@
-// import { useGetAllPeopleQuery, useGetAllPlanetsQuery } from "../redux/swSlice";
+// import { useSelector } from "react-redux";
 
 const Footer = () => {
   const Y = new Date().getFullYear();
-  // const {data: planets} = useGetAllPlanetsQuery()
-  // const {data: people} = useGetAllPeopleQuery()
-
   return (
     <footer className='bg-gray-800 text-gray-200 w-full p-4 flex flex-row justify-between'>
       <span>
         Copyright &copy; {Y} | Andrea La Camera
       </span>
-      <div >
-        {/* { planets && <span>Planets ({planets.results.length})</span>} */}
-        {/* { people && <span> | People ({people.results.length})</span>} */}
-      </div>
     </footer>
   )
 }
